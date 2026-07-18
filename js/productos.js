b34cb8bae9c2ea40086a4f16f2a2cd1e995f5523
bahia-huele-bien <bahiahuelebien@gmail.com>
2026-07-18 12:06:40 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -2,25 +2,25 @@ const productos = [
 
     {
         nombre: "Asad Elixir",
-        precio: 65000,
+        precio: 75000,
         imagen: "img/perfumes/asad-elixir.jpeg"
     },
 
     {
         nombre: "9PM",
-        precio: 60000,
+        precio: 70000,
         imagen: "img/perfumes/9pm.jpeg"
     },
 
     {
         nombre: "Sky Mandarin",
-        precio: 55000,
+        precio: 70000,
         imagen: "img/perfumes/sky-mandarin.jpeg"
     },
 
     {
         nombre: "Khamrah Qahwa",
-        precio: 70000,
+        precio: 65000,
         imagen: "img/perfumes/khamraah-qawha.jpeg"
     },
 
