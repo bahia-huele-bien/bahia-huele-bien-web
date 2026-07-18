1b4a0289ab4d71c6b64493ac1d0a679b104dda2c
bahia-huele-bien <bahiahuelebien@gmail.com>
2026-07-18 11:33:41 -0300

    Create productos.js

---
+++ js/productos.js
@@ -0,0 +1,33 @@
+const productos = [
+
+{
+    nombre: "Asad Elixir",
+    precio: 65000,
+    imagen: "img/perfumes/asad-elixir.jpeg"
+},
+
+{
+    nombre: "9PM",
+    precio: 60000,
+    imagen: "img/perfumes/9pm.jpeg"
+},
+
+{
+    nombre: "Sky Mandarin",
+    precio: 55000,
+    imagen: "img/perfumes/sky-mandarin.jpeg"
+},
+
+{
+    nombre: "Khamrah Qahwa",
+    precio: 70000,
+    imagen: "img/perfumes/khamraah-qawha.jpeg"
+},
+
+{
+    nombre: "Club de Nuit Intense Man",
+    precio: 75000,
+    imagen: "img/perfumes/club-de-nuit-intense.jpeg"
+}
+
+];
